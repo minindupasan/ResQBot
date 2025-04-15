7c08915d2baaa7f7d7b1b2be30f678bb7922ee1f
Minindu Pasan <minindupasan@gmail.com>
2025-04-15 18:27:24 +0530

    Add arduino_secrets.h files for vehicle and room configurations

---
+++ room/include/arduino_secrets.h
@@ -0,0 +1,3 @@
+#define SECRET_DEVICE_KEY "9YEas#yJ?zkMOuVpF@Vodr8wE"
+#define SECRET_SSID "Dialog 4G 181"
+#define SECRET_OPTIONAL_PASS "wifi3515"
---
+++ vehicle/include/arduino_secrets.h
@@ -0,0 +1,2 @@
+#define SECRET_SSID "Dialog 4G 181"
+#define SECRET_PASS "wifi3515"
